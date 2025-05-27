76180be54afab1900ffb04f285e757218d8035f7
Andrii Chornyi <ixxxx@i.ua>
2025-05-27 17:09:15 +0300

    trying to resolve a few UI bugs

--- src/UserWarning.tsx
+++ src/UserWarning.tsx
@@ -4,7 +4,7 @@ export const UserWarning: React.FC = () => (
   <section className="section">
     <p className="box is-size-3">
       Please get your <b> userId </b>{' '}
-      <a href="https://mate-academy.github.io/react_student-registration">
+      <a href="https://mate.academy/students-api/todos?userId=2999">
         here
       </a>{' '}
       and save it in the app <pre>const USER_ID = ...</pre>
---
+++ src/components/ErrorNotification/ErrorNotification.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export const ErrorNotification: React.FC = () => {
+  return (
+    <>
+      {/* DON'T use conditional rendering to hide the notification */}
+      {/* Add the 'hidden' class to hide the message smoothly */}
+      <div
+        data-cy="ErrorNotification"
+        className="notification is-danger is-light has-text-weight-normal"
+      >
+        <button data-cy="HideErrorButton" type="button" className="delete" />
+        {/* show only one message at a time */}
+        Unable to load todos
+        <br />
+        Title should not be empty
+        <br />
+        Unable to add a todo
+        <br />
+        Unable to delete a todo
+        <br />
+        Unable to update a todo
+      </div>
+    </>
+  );
+};
